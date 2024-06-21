3a10a063ed69559b0261966e2b278fdf9542e00f
kaialtair <166514084+altairkaitou@users.noreply.github.com>
2024-06-21 12:57:08 +0700

    Time: 12 ms (99.2%), Space: 11.7 MB (16.89%) - LeetHub

---
+++ 2886-faulty-keyboard/2886-faulty-keyboard.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def finalString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        result = []
+        for char in s:
+            if char == 'i':
+                result.reverse()
+            else:
+                result.append(char)
+        
+        return "".join(result)
+
+
+        
