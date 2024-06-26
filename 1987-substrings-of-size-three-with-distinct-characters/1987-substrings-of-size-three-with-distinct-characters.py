cc6c99ed2326860d01b663d09cb3fb30866bfaf8
kaialtair <166514084+altairkaitou@users.noreply.github.com>
2024-06-26 20:16:27 +0700

    Time: 22 ms (99.72%), Space: 16.6 MB (6.55%) - LeetHub

---
+++ 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        count = 0
+
+        for i in range(len(s) - 1):
+            substring = s[i:i + 3]
+
+            if(len(set(substring)) == 3):
+                count += 1
+
+        return count
+
+        
