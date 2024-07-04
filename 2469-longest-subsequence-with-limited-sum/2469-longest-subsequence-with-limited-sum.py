d869cbce99d46c0ae188423abafea7837f686898
kaialtair <166514084+altairkaitou@users.noreply.github.com>
2024-07-04 20:41:15 +0700

    Time: 104 ms (41.96%), Space: 17 MB (8.01%) - LeetHub

---
+++ 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.py
@@ -0,0 +1,48 @@
+class Solution:
+    def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        
+        result = []
+        all_sum = []
+        sum = 0
+
+        nums.sort()
+
+        for i in range(len(nums)):
+            sum += nums[i]
+            all_sum.append(sum)
+        
+        def bns(all_sum, final):
+            left = 0
+            right = len(all_sum) - 1
+
+            while (left <= right):
+                mid = left + (right - left) // 2
+
+                if all_sum[mid] == final:
+                    return mid
+                elif all_sum[mid] < final:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            
+            if all_sum[mid] > q:
+                return mid - 1
+            return mid 
+
+        for q in queries:
+            q_length = bns(all_sum, q)
+            result.append(q_length + 1)
+
+
+        
+        
+
+        return result
+
+       
+            
+
+
+
+
+        
