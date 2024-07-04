4827027aad8813f90ba2e66b2cb3853b60bc581a
kaialtair <166514084+altairkaitou@users.noreply.github.com>
2024-07-04 20:03:15 +0700

    Time: 95 ms (57.47%), Space: 16.9 MB (24.4%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.py
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.py
@@ -1,22 +1,20 @@
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+
         soldier_count = []
-
-        for i, soldier in enumerate(mat):
-            count = sum(soldier)
-
-            soldier_count.append((soldier, i))
-
-        soldier_count.sort()
-
+        count = 0
         weakest_row = []
 
+        for index, soldier in enumerate(mat):
+            count = sum(soldier)
+
+            soldier_count.append((count, index))
+
+        soldier_count.sort()
+        
         for i in range(k):
             weakest_row.append(soldier_count[i][1])
 
+        
         return weakest_row
-
-
-
-
         
