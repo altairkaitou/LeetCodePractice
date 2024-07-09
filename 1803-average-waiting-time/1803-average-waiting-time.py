e958beb102fe5d82ce9b7d4f956d7e7de03f5f2f
kaialtair <166514084+altairkaitou@users.noreply.github.com>
2024-07-09 19:02:28 +0700

    Time: 788 ms (15.12%), Space: 57.2 MB (9.97%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.py
+++ 1803-average-waiting-time/1803-average-waiting-time.py
@@ -2,10 +2,9 @@ class Solution:
     def averageWaitingTime(self, customers: List[List[int]]) -> float:
         available = 0
         total = 0
-
         for arrival, time in customers:
             available = max(available, arrival) + time
-            total += available - arrival 
+            total += available - arrival
         
         return total / len(customers)
         
