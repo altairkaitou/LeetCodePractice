28a9a5042f495cf95a457705a4fbd88ca9dd4a3e
kaialtair <166514084+altairkaitou@users.noreply.github.com>
2024-07-11 19:30:43 +0700

    Time: 20 ms (30.13%), Space: 11.7 MB (24.66%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.py
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.py
@@ -5,12 +5,13 @@ class Solution(object):
         :rtype: int
         """
         count = 0
-        for i in range (len(nums)):
+
+        for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] == nums[j]:
                     count += 1
-
-        return count 
+        
+        return count
 
 
         
