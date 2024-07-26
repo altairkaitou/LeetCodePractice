6dbda9f707c4b62e16f58c0fc765486631e5006e
kaialtair <166514084+altairkaitou@users.noreply.github.com>
2024-07-26 19:24:16 +0700

    Time: 2 ms (99.83%), Space: 43.3 MB (59.63%) - LeetHub

---
+++ 1472-increasing-decreasing-string/1472-increasing-decreasing-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String sortString(String s) {
+        int[] count = new int[26];
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        StringBuilder result = new StringBuilder();
+        int n = s.length();
+
+        while (result.length() < n) {
+            for(int i = 0; i < 26; i++) {
+                if (count[i] > 0) {
+                    result.append((char) (i + 'a'));
+                    count[i]--;
+                }
+            }
+
+            for(int i = 25; i >= 0; i--) {
+                if (count[i] > 0) {
+                    result.append((char) (i + 'a'));
+                    count[i]--;
+                }
+            }
+        }
+
+        return result.toString();
+
+    
+        
+    }
+}
