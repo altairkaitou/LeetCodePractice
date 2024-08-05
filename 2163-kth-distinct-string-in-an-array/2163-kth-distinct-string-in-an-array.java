0fa916c25434a88949484baf15a6221854c38234
Minh Bao <166514084+altairkaitou@users.noreply.github.com>
2024-08-05 20:56:09 +0700

    Time: 6 ms (91.98%), Space: 44.4 MB (17.65%) - LeetHub

--- 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.java
+++ 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.java
@@ -1,24 +1,19 @@
 class Solution {
     public String kthDistinct(String[] arr, int k) {
-        Map<String, Integer> hash_appear = new HashMap<>();
+        Map<String, Integer> hash_arr = new HashMap<>();
 
-        for (String v : arr) {
-            hash_appear.put(v, hash_appear.getOrDefault(v, 0) + 1);
+        for(String v : arr) {
+            hash_arr.put(v, hash_arr.getOrDefault(v, 0) + 1);
         }
 
-        for (String v : arr) {
-            if (hash_appear.get(v) == 1) {
+        for(String v : arr) {
+            if (hash_arr.get(v) == 1) {
                 k--;
-
                 if (k == 0) {
                     return v;
                 }
             }
         }
-
-        return ""; 
-
-
-        
+        return "";
     }
 }
