9c57e0cda741a0da068b7034163351733247a111
Minh Bao <166514084+altairkaitou@users.noreply.github.com>
2024-09-23 08:43:36 +0700

    Time: 46 ms (42.18%), Space: 45.4 MB (36.39%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.java
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.java
@@ -4,24 +4,22 @@ class Solution {
         Set<String> wordSet = new HashSet<>(Arrays.asList(dictionary));
 
         int[] dp = new int[n + 1];
-
         Arrays.fill(dp, n + 1);
-        dp[0] =  0;
+
+        dp[0] = 0;
 
         for(int i = 1; i <= n; i++) {
-            for(int j = 0; j < i; j++) {
-                String subs = s.substring(j, i);
-
-                if(wordSet.contains(subs)) {
+            for(int j = 0; j < i;j++) {
+                String subs = s.substring(j,i);
+                if (wordSet.contains(subs)) {
                     dp[i] = Math.min(dp[i], dp[j]);
                 } else {
                     dp[i] = Math.min(dp[i], dp[j] + (i - j));
                 }
             }
-
         }
 
         return dp[n];
-
+        
     }
 }
