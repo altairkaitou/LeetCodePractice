0c472d2abe8fe212bd2c684afcfcd7b40228cbe3
Minh Bao <166514084+altairkaitou@users.noreply.github.com>
2026-03-27 15:51:43 +0700

    Time: 1 ms (68.97%), Space: 55.6 MB (89.66%) - LeetHub

---
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} mat
+ * @param {number} k
+ * @return {boolean}
+ */
+var areSimilar = function(mat, k) {
+    const m = mat.length;
+    const n = mat[0].length;
+    k %= n;
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (i % 2 === 0) {
+                if (mat[i][j] !== mat[i][(j + k) % n]) {
+                    return false;
+                }
+            } else {
+                if (mat[i][j] !== mat[i][(j - k + n) % n]) {
+                    return false;
+                }
+            }
+
+        }
+    }
+    return true;
+    
+};
